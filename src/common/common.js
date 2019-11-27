008e25d30b76a12bd9ea3dd5b7960c429cd0a4f3
Kacoo <kacoo1205@qq.com>
2019-11-28 00:54:20 +0800

    前端存储一波副本id和name

---
+++ src/common/common.js
@@ -0,0 +1,12 @@
+const instance = [
+  { id: 1, name: '收集礼带', },
+  { id: 2, name: '消灭蝙蝠', },
+  { id: 3, name: '收集宝箱', },
+  { id: 4, name: '切西瓜', },
+  { id: 5, name: '收集金币', },
+  { id: 6, name: '消灭河豚', },
+  { id: 7, name: '摧毁飞镖', },
+  { id: 8, name: '无尽狂奔', },
+]
+
+export default instance;
